f541d1958633638a9d1b58d6da98b46a7b0e2b04
Данила Удовиченко <danyarusskiy17@mail.ru>
2023-10-30 14:24:59 +0400

    upd settings v2

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -3,9 +3,9 @@ import os
 import dotenv
 from pathlib import Path
 
-dotenv.load_dotenv()
-
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+dotenv.load_dotenv(os.path.join(BASE_DIR, '.env'))
 
 SECRET_KEY = os.getenv('SECRET_KEY')
 
