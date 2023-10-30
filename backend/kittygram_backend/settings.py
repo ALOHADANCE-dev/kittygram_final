35449417176b42e98b30b0e3ae4a9d78e8e22cef
Данила Удовиченко <danyarusskiy17@mail.ru>
2023-10-30 15:27:04 +0400

    update settings

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -1,17 +1,13 @@
 import os
 from pathlib import Path
 
-import dotenv
-
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-dotenv.load_dotenv()
+SECRET_KEY = 'django-insecure-cg6*%6d51ef8f#4!r3*$vmxm4)abgjw8mo!4y-q*uq1!4$-89$'
 
-SECRET_KEY = os.getenv('SECRET_KEY')
+DEBUG = True
 
-DEBUG = os.getenv('DEBUG', 'False') == 'True'
-
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS')
+ALLOWED_HOSTS = ['158.160.14.35', '127.0.0.1', 'localhost', 'yggdora-project.ddns.net']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
