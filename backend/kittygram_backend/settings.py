b60b074aeed656eddfb09cd483e565f53376d4ed
Данила Удовиченко <danyarusskiy17@mail.ru>
2023-10-31 17:33:15 +0400

    add secret keys in settings

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -6,11 +6,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Я хотел убрал эти переменные в .env, но не проходили джанго тесты при деплое
 # и я не знаю как это пофиксить, можно ли в теории их в секреты добавить?
 
-SECRET_KEY = 'django-insecure-cg6*%6d51ef8f#4!r3*$vmxm4)abgjw8mo!4y-q*uq1!4$-89$'
+SECRET_KEY = os.getenv('SECRET_KEY', 'heresomsesecretkey')
 
-DEBUG = False
+DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = ['158.160.14.35', '127.0.0.1', 'localhost', 'yggdora-project.ddns.net']
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', ['127.0.0.1', 'localhost'])
 
 INSTALLED_APPS = [
     'django.contrib.admin',
